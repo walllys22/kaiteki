390560d766bedd18f3c2c890c5f588ef5054c37f
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-05-06 22:53:20 -0400

    dev

--- resources/views/alumnos/partials/certificadoExamenLjp.blade.php
+++ resources/views/alumnos/partials/certificadoExamenLjp.blade.php
@@ -102,6 +102,8 @@
         }
         .line {
             display: block;
+            text-align: justify;
+            text-align-last: left;
         }
         @media print {
             body {
