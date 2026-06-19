27213d03d16d59bc744765ab4f05e6b5b112174a
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-06-19 00:22:27 -0400

    dev

--- resources/views/vendor/voyager/formfields/image.blade.php
+++ resources/views/vendor/voyager/formfields/image.blade.php
@@ -1,8 +1,15 @@
 @if(isset($dataTypeContent->{$row->field}))
     <div data-field-name="{{ $row->field }}">
         <a href="#" class="voyager-x remove-single-image" style="position:absolute;"></a>
-        <img src="@if( !filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $dataTypeContent->{$row->field} ) }}@else{{ $dataTypeContent->{$row->field} }}@endif"
-          data-file-name="{{ $dataTypeContent->{$row->field} }}" data-id="{{ $dataTypeContent->getKey() }}"
+        @php
+            $rawImg = $dataTypeContent->{$row->field};
+            $isUrl = filter_var($rawImg, FILTER_VALIDATE_URL);
+            $croppedKey = str_replace('.avif', '', $rawImg) . '-cropped.webp';
+            $imgSrc = $isUrl ? $rawImg : \Storage::disk(env('FILESYSTEM_DRIVER'))->url($croppedKey);
+        @endphp
+        <img src="{{ $imgSrc }}"
+          onerror="this.onerror=null; this.src='{{ $isUrl ? $rawImg : \Storage::disk(env('FILESYSTEM_DRIVER'))->url($rawImg) }}'; this.setAttribute('data-fallback','1');"
+          data-file-name="{{ $rawImg }}" data-id="{{ $dataTypeContent->getKey() }}"
           style="max-width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;">
     </div>
 @endif
