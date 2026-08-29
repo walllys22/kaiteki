ead46e714a0e7a113dc82fa830acafffeb597e4d
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-08-29 13:22:32 -0400

    dev

--- resources/views/administrations/people/list.blade.php
+++ resources/views/administrations/people/list.blade.php
@@ -124,7 +124,7 @@
                 </tr>
                 @empty
                     <tr>
-                        <td colspan="6">
+                        <td colspan="7">
                             <h5 class="text-center" style="margin-top: 50px">
                                 <img src="{{ asset('images/empty.png') }}" width="120px" alt="" style="opacity: 0.8">
                                 <br><br>
--- resources/views/alumnos/list.blade.php
+++ resources/views/alumnos/list.blade.php
@@ -137,7 +137,7 @@
                 </tr>
                 @empty
                     <tr>
-                        <td colspan="8">
+                        <td colspan="9">
                             <h5 class="text-center" style="margin-top: 50px">
                                 <img src="{{ asset('images/empty.png') }}" width="120px" alt="" style="opacity: 0.8">
                                 <br><br>
